283fe59191961763e684aa0961af243f124eb3e4
abdulazizgurishan-design <abdulaziz.gurishan@gmail.com>
2026-05-21 23:34:46 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,2 +1,3 @@
-import Radar from "../components/Radar";
+import dynamic from "next/dynamic";
+const Radar = dynamic(() => import("../components/Radar"), { ssr: false });
 export default function Home() { return <Radar />; }
