0e86fe2ba86a589a065d7ab97263e8fe11abf050
abdulazizgurishan-design <abdulaziz.gurishan@gmail.com>
2026-05-28 16:22:35 +0300

    Update scan.js

--- pages/api/scan.js
+++ pages/api/scan.js
@@ -1,7 +1,17 @@
-const POLYGON_KEY = "Kv7F3MSRRgrH_8dOoFu4L0TpFO23Trix";
+const POLYGON_KEY = process.env.POLYGON_API_KEY;
 const BASE = "https://api.polygon.io";
 
 const WATCHLIST = [
+  // ✅ أسهم قيادية - تكنولوجيا وذكاء اصطناعي
+  "NVDA","AMD","MSFT","META","GOOGL","AMZN","AAPL","TSLA","PLTR","SMCI",
+  // ✅ أسهم قيادية - بيوتك وصحة
+  "MRNA","BNTX","VRTX","REGN","ILMN",
+  // ✅ أسهم قيادية - طاقة ومعادن
+  "ENPH","FSLR","MP","PLUG","CHPT",
+  // ✅ أسهم قيادية - تداول وتشفير
+  "COIN","MSTR","HOOD","SOFI","UPST",
+  // ✅ أسهم قيادية - مركبات كهربائية ومستقبل
+  "RIVN","LCID","JOBY","RKLB","ARKK",
   // Batch 1
   "SOUN","BBAI","KULR","CRKN","NKLA","MULN","WISA","CBAT","BFRI","ATXS",
   "HOLO","BHAT","CLSK","MARA","RIOT","CIFR","BTBT","IREN","ARBK","MIGI",
@@ -45,7 +55,7 @@ const WATCHLIST = [
   "BHIL","BIAF","BIGC","BILI","BIMI","BIOX","BIRD","BITE","BIVI","BJDX",
   "BKCC","BKFG","BKKT","BKSY","BKTI","BLBD","BLBX","BLDP","BLDR","BLFS",
   "BLKB","BLMN","BLNK","BLPH","BLRX","BLTE","BLUE","BLZE","BMBL","BMRA",
-  "BMRC","BMTX","BNGO","BNIX","BNRG","BNSO","BNTC","BNTX","BOCN","BOLT",
+  "BMRC","BMTX","BNGO","BNIX","BNRG","BNSO","BNTC","BOCN","BOLT",
   "BONE","BONT","BOOM","BORR","BOTJ","BPMC","BPOP","BPRN","BPTS","BPTH",
   "BRAC","BRAG","BRDG","BRDS","BREA","BRFS","BRID","BRKH","BRLT","BRMK",
   "BROG","BRTX","BRWC","BRWS","BSFC","BSGM","BSRR","BSVN","BTAI","BTBT",
@@ -58,10 +68,9 @@ const WATCHLIST = [
   "CDXS","CDZI","CEAD","CECO","CELC","CELH","CELU","CELZ","CEMI","CENT",
   "CERO","CERS","CERT","CEVA","CFBK","CFFI","CFFE","CFLT","CFNB","CFRX",
   "CGEM","CGNT","CGNX","CGON","CGRN","CGRO","CGTX","CHCI","CHCT","CHDN",
-  "CHEK","CHMG","CHRD","CHRS","CHRW","CHSCL","CHSCO","CHSCM","CHSCN","CHSCP",
-  "CHTR","CHUY","CIFR","CIGI","CIMN","CINC","CING","CINT","CIVB","CIZN",
-  "CJET","CKPT","CKVN","CLBK","CLBS","CLDT","CLFD","CLGN","CLIR","CLMB",
-  "CLMT","CLNE","CLNN","CLNV","CLOA","CLOE","CLOV","CLPR","CLPS","CLPT",
+  "CHEK","CHMG","CHRD","CHRS","CHRW","CIFR","CIGI","CIMN","CINC","CING",
+  "CINT","CIVB","CIZN","CJET","CKPT","CKVN","CLBK","CLBS","CLDT","CLFD",
+  "CLGN","CLIR","CLMB","CLMT","CLNE","CLNN","CLNV","CLOV","CLPR","CLPS",
   "CLRB","CLRO","CLSD","CLSK","CLST","CLVR","CLVT","CLWT","CMBT","CMCO",
   "CMCT","CMDV","CMLS","CMMB","CMND","CMPO","CMPS","CMRX","CMTG","CMTS",
   // Batch 5
@@ -76,60 +85,88 @@ const WATCHLIST = [
   "CSTE","CSTL","CSTR","CSWC","CSWI","CTGO","CTIB","CTIC","CTLT","CTMX",
   "CTON","CTOS","CTRA","CTRE","CTRL","CTSO","CTXR","CTXS","CUBS","CUEN",
   // Batch 6
-  "DARE","DBGI","DBVT","DCBO","DCFC","DCGO","DCOM","DCTH","DDDD","DDIV",
-  "DDOG","DELT","DEMO","DENN","DERA","DGHI","DGII","DGLY","DGNX","DGNU",
-  "DGTI","DHIL","DHTX","DIBS","DIGS","DIOD","DIST","DJCO","DKNG","DLHC",
-  "DLPN","DLTH","DLTX","DMAC","DMAR","DMEI","DMRC","DMTK","DNAA","DNAB",
-  "DNAC","DNAD","DNAE","DNAF","DNAG","DNAH","DNAI","DNAJ","DNJR","DNLI",
-  "DNMR","DNNGY","DNOW","DNUT","DOMO","DOOO","DORM","DOUG","DOVA","DPCS",
-  "DPSI","DRAY","DRCT","DRNA","DRRX","DRTS","DRTS","DRVN","DSAQ","DSGN",
-  "DSGX","DSKE","DSNY","DSON","DSPC","DSSI","DSWL","DTEA","DTIL","DTSS",
-  "DUET","DUOL","DURO","DXPE","DXYN","DYAI","DYFN","DYNS","DYNT","DZSI",
+  "DARE","DBGI","DBVT","DCBO","DCFC","DCGO","DCOM","DCTH","DDOG","DELT",
+  "DEMO","DENN","DERA","DGHI","DGII","DGLY","DGNX","DGNU","DGTI","DHIL",
+  "DHTX","DIBS","DIGS","DIOD","DIST","DJCO","DKNG","DLHC","DLPN","DLTH",
+  "DLTX","DMAC","DMAR","DMEI","DMRC","DMTK","DNLI","DNMR","DNOW","DNUT",
+  "DOMO","DOOO","DORM","DOUG","DOVA","DPCS","DPSI","DRAY","DRCT","DRNA",
+  "DRRX","DRVN","DSAQ","DSGN","DSGX","DSKE","DSON","DSPC","DSSI","DSWL",
+  "DTEA","DTIL","DTSS","DUET","DUOL","DURO","DXPE","DXYN","DYAI","DYNS",
   // Batch 7
-  "EACO","EARN","EAST","EBIX","EBMT","EBSB","EBTC","ECBK","ECOR","ECPG",
-  "EDBL","EDGW","EDIT","EDRY","EDSA","EDTK","EDTX","EEMA","EFOI","EFSH",
-  "EGAN","EGBN","EGHT","EGIO","EGLT","EGRX","EIDA","EKSO","ELEV","ELSE",
-  "ELST","ELVA","EMBC","EMKR","EMLD","EMMS","EMTX","ENCP","ENDP","ENER",
-  "ENFN","ENGS","ENLT","ENLV","ENOV","ENPC","ENPH","ENSC","ENSG","ENVA",
-  "ENVB","ENVX","EOLS","EOSE","EPAC","EPAZ","EPIX","EPOW","EPSN","EPZM",
-  "EQBK","EQNR","EQRX","ERAS","ERES","ERIC","ERIE","ERII","ERNA","ERNZ",
-  "ESEA","ESGR","ESLA","ESMT","ESNT","ESPR","ESSA","ESTC","ESXB","ETNB",
-  "ETSY","ETTX","EVAX","EVBG","EVBN","EVGO","EVGR","EVIO","EVLV","EVMO",
-  "EVOK","EVOP","EVTL","EVTV","EWCZ","EXFY","EXLS","EXPI","EXTO","EYEG",
+  "EACO","EARN","EAST","EBIX","EBMT","EBTC","ECBK","ECOR","ECPG","EDBL",
+  "EDIT","EDRY","EDSA","EFOI","EFSH","EGAN","EGBN","EGHT","EGIO","EGLT",
+  "EGRX","EKSO","ELEV","ELSE","ELVA","EMBC","EMKR","EMMS","EMTX","ENER",
+  "ENFN","ENGS","ENLT","ENLV","ENOV","ENPH","ENSC","ENSG","ENVA","ENVB",
+  "ENVX","EOLS","EOSE","EPAZ","EPIX","EPOW","EPZM","EQBK","ERAS","ERES",
+  "ERIC","ERII","ESEA","ESMT","ESNT","ESPR","ESSA","ESTC","ETNB","ETSY",
+  "EVAX","EVBG","EVGO","EVIO","EVLV","EVMO","EVOK","EVTL","EVTV","EXFY",
   // Batch 8
-  "FBIO","FBIZ","FBLG","FBMS","FBNC","FBRT","FBSS","FCAP","FCEL","FCNCA",
-  "FCNCO","FCPT","FCRD","FCUV","FDBC","FDEF","FDMT","FDUS","FEAT","FEIM",
-  "FELE","FELP","FEMY","FEPI","FERN","FGEN","FGMC","FGNA","FHTX","FIAC",
-  "FIBK","FIII","FINV","FIXX","FKWL","FLGC","FLIC","FLLD","FLME","FLNC",
-  "FLNT","FLOC","FLOT","FLPX","FLRN","FLWS","FLXS","FMBI","FMCC","FMNB",
-  "FMST","FMTO","FNAM","FNCH","FNCX","FNKO","FNLC","FNMA","FNRN","FNTX",
-  "FOLD","FONR","FORD","FORE","FORM","FORR","FORTY","FOSL","FPAY","FPHF",
-  "FPXI","FRBA","FRBK","FRGE","FRGT","FRLN","FROG","FRPH","FRPT","FRST",
-  "FRSX","FRTA","FRWT","FSFG","FSLR","FSRX","FSTR","FTCI","FTFT","FTHM",
-  "FTIV","FTLF","FTPA","FTRE","FTRP","FTSI","FTUK","FUBO","FUES","FULT",
+  "FBIO","FBLG","FBMS","FBNC","FBRT","FCAP","FCEL","FCPT","FDBC","FDEF",
+  "FDMT","FEIM","FELE","FGEN","FHTX","FIBK","FINV","FIXX","FKWL","FLGC",
+  "FLIC","FLLD","FLME","FLNC","FLNT","FLWS","FLXS","FMNB","FNKO","FNLC",
+  "FNMA","FNTX","FOLD","FONR","FORE","FORM","FORR","FOSL","FRBA","FRBK",
+  "FRGE","FRGT","FRLN","FROG","FRPH","FRPT","FRST","FRSX","FRTA","FSFG",
+  "FSLR","FSTR","FTCI","FTFT","FTHM","FTLF","FTRE","FTSI","FUBO","FULT",
   // Batch 9
-  "GALT","GATO","GBOX","GCAC","GCBC","GDEN","GDOT","GDST","GDYN","GECC",
-  "GENC","GENI","GENQ","GEOS","GERN","GFAI","GFMD","GGAA","GGAL","GHIX",
-  "GHRS","GHSI","GIFI","GILT","GIMI","GKOS","GLAD","GLBE","GLBS","GLDD",
-  "GLMD","GLNG","GLPG","GLRE","GLSI","GLTO","GLTX","GLUE","GLXG","GMBL",
-  "GMDA","GMFI","GMGI","GMRE","GNFT","GNLN","GNMK","GNPX","GNRS","GNSS",
-  "GNTX","GNTY","GNUS","GOBI","GOEV","GOED","GOGL","GOGO","GOOD","GOOG",
-  "GOPI","GORV","GOSS","GOVX","GPAC","GPAK","GPAT","GPCO","GPCR","GPEX",
-  "GPMT","GPOR","GPRE","GPRO","GPRK","GRAM","GRDI","GREE","GREI","GRIL",
-  "GRIN","GRMN","GRNV","GRPN","GRTS","GRTX","GRVY","GRWG","GSAT","GSBC",
-  "GSIT","GSMG","GSUN","GTBP","GTEC","GTLB","GTLS","GTNX","GURE","GWAV",
+  "GALT","GATO","GBOX","GCBC","GDEN","GDOT","GDYN","GENC","GENI","GEOS",
+  "GERN","GFAI","GGAL","GHRS","GHSI","GIFI","GILT","GIMI","GLAD","GLBE",
+  "GLBS","GLDD","GLMD","GLNG","GLPG","GLRE","GLSI","GLTO","GLTX","GLUE",
+  "GMBL","GMDA","GMRE","GNLN","GNMK","GNPX","GNSS","GNTX","GNTY","GNUS",
+  "GOEV","GOED","GOGL","GOGO","GOOD","GOPI","GORV","GOSS","GOVX","GPCR",
+  "GPMT","GPOR","GPRE","GPRO","GRAM","GREE","GRIL","GRIN","GRMN","GRPN",
+  "GRTS","GRTX","GRVY","GRWG","GSAT","GSBC","GSIT","GSMG","GTLB","GTLS",
   // Batch 10
-  "HAIN","HALL","HALO","HAPP","HARP","HAYN","HBAN","HBCP","HBIO","HBMD",
-  "HCAT","HCCI","HCDI","HCKT","HCNW","HCSG","HCWB","HDCO","HDSN","HEAR",
-  "HEES","HEPA","HEPS","HERA","HERO","HEXO","HFBL","HFFG","HFWA","HGBL",
-  "HGEN","HGTY","HHGC","HIBB","HIFS","HIHO","HIIQ","HIMAX","HIMS","HIPO",
-  "HITI","HIVE","HJLI","HKIT","HLIO","HLLY","HLNE","HLTH","HLVX","HMST",
-  "HNRG","HNNAZ","HNNA","HOLO","HOLX","HOMB","HONE","HONR","HOOK","HOTH",
-  "HOVNP","HOWL","HPCO","HPKN","HPNN","HPRO","HPSN","HRMY","HROW","HRYU",
-  "HSDT","HSII","HSKA","HSON","HSPX","HTBI","HTBK","HTGM","HTIA","HTLD",
-  "HTLF","HTOO","HTPA","HUDI","HUIZ","HUMA","HURC","HUSN","HVBC","HWBK",
-  "HWKN","HYAC","HYFM","HYLN","HYMC","HYPR","HYSR","HYZN","HZNP","HZON"
+  "HAIN","HALL","HALO","HARP","HAYN","HBCP","HBIO","HCAT","HCCI","HCDI",
+  "HCSG","HCWB","HDSN","HEAR","HEES","HEPA","HEPS","HERO","HEXO","HFWA",
+  "HGEN","HIBB","HIFS","HIIQ","HIMS","HIPO","HITI","HIVE","HKIT","HLLY",
+  "HLTH","HLVX","HNRG","HOLO","HOLX","HOMB","HONE","HOOK","HOTH","HPCO",
+  "HPSN","HRMY","HROW","HSII","HSKA","HSON","HTBK","HTGM","HTLD","HTLF",
+  "HUDI","HUMA","HURC","HVBC","HWBK","HWKN","HYAC","HYLN","HYMC","HYPR"
 ];
+
+async function getEMA(ticker, window) {
+  try {
+    const url = `${BASE}/v1/indicators/ema/${ticker}?timespan=day&adjusted=true&window=${window}&series_type=close&order=desc&limit=1&apiKey=${POLYGON_KEY}`;
+    const r = await fetch(url);
+    if (!r.ok) return null;
+    const d = await r.json();
+    return d?.results?.values?.[0]?.value ?? null;
+  } catch { return null; }
+}
+
+async function getRSI(ticker) {
+  try {
+    const url = `${BASE}/v1/indicators/rsi/${ticker}?timespan=day&adjusted=true&window=14&series_type=close&order=desc&limit=1&apiKey=${POLYGON_KEY}`;
+    const r = await fetch(url);
+    if (!r.ok) return null;
+    const d = await r.json();
+    return d?.results?.values?.[0]?.value ?? null;
+  } catch { return null; }
+}
+
+async function getFibLevels(ticker) {
+  try {
+    const today = new Date().toISOString().split('T')[0];
+    const before = new Date();
+    before.setDate(before.getDate() - 30);
+    const from = before.toISOString().split('T')[0];
+    const url = `${BASE}/v2/aggs/ticker/${ticker}/range/1/day/${from}/${today}?adjusted=true&sort=asc&limit=30&apiKey=${POLYGON_KEY}`;
+    const r = await fetch(url);
+    if (!r.ok) return null;
+    const d = await r.json();
+    const bars = d?.results;
+    if (!bars || bars.length < 5) return null;
+    const high = Math.max(...bars.map(b => b.h));
+    const low  = Math.min(...bars.map(b => b.l));
+    const diff = high - low;
+    return {
+      fib618: parseFloat((high - diff * 0.618).toFixed(2)),
+      fib500: parseFloat((high - diff * 0.500).toFixed(2)),
+      fib382: parseFloat((high - diff * 0.382).toFixed(2)),
+      high, low
+    };
+  } catch { return null; }
+}
 
 export default async function handler(req, res) {
   try {
@@ -141,46 +178,55 @@ export default async function handler(req, res) {
     const isPreMarket = !isWeekend && h >= 4 && (h < 9 || (h === 9 && m < 30));
     const MIN_VOLUME  = isPreMarket ? 5000 : 20000;
 
+    const uniqueList = [...new Set(WATCHLIST)];
     const CHUNK_SIZE = 100;
     const chunks = [];
-    for (let i = 0; i < WATCHLIST.length; i += CHUNK_SIZE) {
-      chunks.push(WATCHLIST.slice(i, i + CHUNK_SIZE));
+    for (let i = 0; i < uniqueList.length; i += CHUNK_SIZE) {
+      chunks.push(uniqueList.slice(i, i + CHUNK_SIZE));
     }
 
     const allTickers = [];
-    await Promise.all(
-      chunks.map(async (chunk) => {
-        try {
-          const url = `${BASE}/v2/snapshot/locale/us/markets/stocks/tickers?tickers=${chunk.join(",")}&apiKey=${POLYGON_KEY}`;
-          const r = await fetch(url);
-          if (!r.ok) return;
-          const d = await r.json();
-          if (d?.tickers) allTickers.push(...d.tickers);
-        } catch { }
-      })
-    );
+    await Promise.all(chunks.map(async (chunk) => {
+      try {
+        const url = `${BASE}/v2/snapshot/locale/us/markets/stocks/tickers?tickers=${chunk.join(",")}&apiKey=${POLYGON_KEY}`;
+        const r = await fetch(url);
+        if (!r.ok) return;
+        const d = await r.json();
+        if (d?.tickers) allTickers.push(...d.tickers);
+      } catch { }
+    }));
 
-    const finalResults = [];
-
+    // فلتر أولي
+    const candidates = [];
     for (const data of allTickers) {
-      const ticker = data.ticker;
-
       let price  = data.min?.c ?? data.lastTrade?.p ?? data.day?.c ?? 0;
       let volume = data.day?.v ?? 0;
-
       if (volume === 0 && data.prevDay) {
         price  = data.prevDay.c || price;
         volume = data.prevDay.v || 0;
       }
-
       if (price < 0.5 || price > 50) continue;
       if (volume < MIN_VOLUME) continue;
-
       const prevClose = data.prevDay?.c || price;
       const changePct = prevClose ? ((price - prevClose) / prevClose) * 100 : 0;
-
-      // فلتر: استبعاد الأسهم التي ارتفعت أكثر من 20% في نفس اليوم
       if (changePct > 20) continue;
+      candidates.push({ data, price, volume, prevClose, changePct });
+    }
+
+    // أفضل 50 بالحجم للحصول على المؤشرات
+    const top50 = candidates.sort((a, b) => b.volume - a.volume).slice(0, 50);
+
+    const finalResults = [];
+
+    await Promise.all(top50.map(async ({ data, price, volume, prevClose, changePct }) => {
+      const ticker = data.ticker;
+
+      const [ema9, ema20, rsi, fibLevels] = await Promise.all([
+        getEMA(ticker, 9),
+        getEMA(ticker, 20),
+        getRSI(ticker),
+        getFibLevels(ticker),
+      ]);
 
       const vwap      = data.day?.vw || price;
       const aboveVWAP = price > vwap;
@@ -201,58 +247,86 @@ export default async function handler(req, res) {
       const reward   = target1 - price;
       const rr       = risk > 0 ? (reward / risk).toFixed(1) : "0";
 
-      if (!isPreMarket && parseFloat(rr) < 1.0) continue;
+      if (!isPreMarket && parseFloat(rr) < 1.0) return;
 
       let score = 30;
+
+      // حجم التداول
       if (volume > 2_000_000)     score += 25;
       else if (volume > 500_000)  score += 18;
       else if (volume > 100_000)  score += 12;
       else if (volume > 50_000)   score += 6;
 
+      // نسبة التغيير
       if (changePct > 15)         score += 20;
       else if (changePct > 10)    score += 15;
       else if (changePct > 5)     score += 10;
       else if (changePct > 2)     score += 5;
       else if (changePct < 0)     score -= 5;
 
+      // VWAP
       if (aboveVWAP)              score += 15;
 
+      // Gap الصباحي
       if (preGap > 10)            score += 10;
       else if (preGap > 5)        score += 7;
       else if (preGap > 2)        score += 4;
 
+      // R/R
       const rrNum = parseFloat(rr);
       if (rrNum >= 3)             score += 10;
       else if (rrNum >= 2)        score += 6;
       else if (rrNum >= 1.5)      score += 3;
 
-      score = Math.max(30, Math.min(score, 99));
+      // ✅ EMA Cross
+      if (ema9 && ema20 && ema9 > ema20) score += 10;
 
-      if (score < 55) continue;
+      // ✅ RSI
+      if (rsi !== null) {
+        if (rsi >= 50 && rsi <= 70)  score += 10;
+        else if (rsi > 70)           score -= 5;
+        else if (rsi < 40)           score -= 8;
+      }
+
+      // ✅ فيبوناتشي
+      if (fibLevels) {
+        if (price > fibLevels.fib618)      score += 8;
+        else if (price > fibLevels.fib500) score += 5;
+        else if (price > fibLevels.fib382) score += 3;
+      }
+
+      score = Math.max(30, Math.min(score, 99));
+      if (score < 55) return;
 
       const confidence =
         score >= 85 ? "💥 قوة قصوى" :
         score >= 70 ? "🔥 إشارة ممتازة" : "👀 مراقبة";
 
       finalResults.push({
-        symbol:     ticker,
-        price:      parseFloat(price.toFixed(2)),
+        symbol:    ticker,
+        price:     parseFloat(price.toFixed(2)),
         change_pct: parseFloat(changePct.toFixed(2)),
         volume,
         rr,
-        signal:     confidence,
+        signal:    confidence,
         score,
-        marketCap:  data.marketCap ? data.marketCap / 1_000_000 : null,
+        marketCap: data.marketCap ? data.marketCap / 1_000_000 : null,
+        ema9:      ema9  ? parseFloat(ema9.toFixed(2))  : null,
+        ema20:     ema20 ? parseFloat(ema20.toFixed(2)) : null,
+        rsi:       rsi   ? parseFloat(rsi.toFixed(1))   : null,
+        vwap:      parseFloat(vwap.toFixed(2)),
+        rvol:      parseFloat((volume / 500000).toFixed(1)),
+        fib:       fibLevels || null,
         levels: { sl: stopLoss, t1: target1, t2: target2, t3: target3, slPct, risk }
       });
-    }
+    }));
 
     finalResults.sort((a, b) => b.score - a.score || b.volume - a.volume);
 
     return res.status(200).json({
       success: true,
       results: finalResults.slice(0, 25),
-      total:   WATCHLIST.length
+      total:   uniqueList.length
     });
 
   } catch (error) {
